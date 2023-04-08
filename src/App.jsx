8c92cdbd65183361f4b2495bd3433af44bcac05e
Desto4u <desto4q@gmail.com>
2023-04-08 16:49:10 +0100

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React, { useLayoutEffect } from 'react'
+import React, { useContext, useLayoutEffect } from 'react'
 import WebFont from 'webfontloader';
 import Router from './Router/Router';
+import "./App.scss"
+import { userContext } from './context/Context';
 WebFont.load({
   google: {
     families: ['Droid Sans', 'Droid Serif', "Montserrat"]
@@ -11,8 +13,11 @@ function App() {
   useLayoutEffect(()=>{
     let  header = document.querySelector("title").innerHTML= "INsider"
   })
+  let {theme} = useContext(userContext)
+  console.log(theme)
   return (
-    <div className="App">
+    <div className="App" id={theme}>
+
       <Router/>
     </div>
   )
---
+++ src/context/Context.jsx
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from 'react'
+
+export let userContext = createContext()
+function Context({Children}) {
+    let [theme,setTheme] = useState("light")
+    let toggletheme = ()=>{
+        setTheme((them)=>{
+            {them == "light" ? "dark" : "light"}
+        })
+    }
+    let values = {theme,toggletheme}
+  return (
+    <userContext.Provider value={values}>
+        {Children}
+    </userContext.Provider>
+  )
+}
+
+export default Context
